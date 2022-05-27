b1ebdf3b0ffdcbb7eba45532a15e3fce062ea6ef
joshuajeschek <joshua@jeschek.eu>
2022-05-27 11:04:22 +0200

    refactor(cli): change token checking to for better ux
    added status feedback and better prompts

--- bonsaidiary/cli.py
+++ bonsaidiary/cli.py
@@ -2,7 +2,8 @@ import keyring
 import os
 import yaml
 import requests
-from typing import Any, Dict, Optional
+import getpass
+from typing import Any, Dict, Tuple, Optional
 from github import Github, GithubException
 
 SERVICE = 'bonsaidiary'
@@ -20,7 +21,7 @@ def save_config(config: dict, path: str = CONFIG_PATH):
         yaml.dump(config, f)
 
 def load_config(path: str = CONFIG_PATH) -> dict:
-    """Load user config."""
+    '''Load user config.'''
     try:
         with open(path, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
@@ -28,17 +29,23 @@ def load_config(path: str = CONFIG_PATH) -> dict:
         return {}
     return config
 
-def is_valid_pat(pat: str) -> bool:
+def is_valid_pat(pat: str) -> Tuple[int, str]:
+    '''checks the GitHub Personal Access Token'''
     res = requests.get('https://api.github.com/user', headers={'Authorization': f'token {pat}'})
     if res.status_code != 200:
-        return False
-    return 'repo' in res.headers.get('x-oauth-scopes')
+        return res.status_code, res.reason
+    if  'repo' not in res.headers.get('x-oauth-scopes'):
+        return 401, 'missing repo scope'
+    return ()
 
 def get_gh_pat() -> Optional[str]:
-    """read GitHub Personal Access Token from keyring or user"""
+    '''read GitHub Personal Access Token from keyring or user'''
     pat = keyring.get_password(SERVICE, str(os.getuid()))
-    while not is_valid_pat(pat):
-        pat = input('Could not find a valid Personal Access Token for GitHub. \nPlease provide one with repo scope: ')
+    pat = getpass.getpass('Please provide a GitHub Personal Access Token with repo scope: ') if not pat else pat
+    while status := is_valid_pat(pat):
+        if status[0] != 401:
+            return print(f'[error] {status[1]}')
+        pat = getpass.getpass(f'There is an error with your GitHub PAT ({status[1]}). \nPlease provide a valid token: ')
     keyring.set_password(SERVICE, str(os.getuid()), pat)
     return pat
 
@@ -52,6 +59,8 @@ def main():
         save_config(config)
 
     pat = get_gh_pat()
+    if not pat:
+        exit(-1)
     gh = Github(pat)
 
 if __name__ == '__main__':
